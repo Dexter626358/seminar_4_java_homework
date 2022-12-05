fa3435796960d5b850c82f6c685db53600e146fa
Dexter <gusevsergey85@gmail.com>
2022-12-05 15:16:01 +0300

    Enqueue and dequeue

--- src/main/java/task2.java
+++ src/main/java/task2.java
@@ -1,5 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class task2 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Queue<String> livingPresidents = new LinkedList<>();
+        livingPresidents.add("Дуайт Эйзенхауэр");
+        livingPresidents.add("Джон Кеннеди");
+        livingPresidents.add("Линдон Джонсон");
+        livingPresidents.add("Ричард Никсон");
+        livingPresidents.add("Джеральд Форд");
+        livingPresidents.add("Джимми Картер");
+        livingPresidents.offer("Рональд Рейган");
+        livingPresidents.offer("Джордж Герберт Уокер Буш");
+        livingPresidents.add("Билл Клинтон");
+        livingPresidents.add("Джордж Уокер Буш");
+        livingPresidents.add("Барак Обама");
+        livingPresidents.add("Дональд Трамп");
+        livingPresidents.add("Джо Байден");
+        System.out.println("Президенты США");
+        System.out.println(livingPresidents);
+        livingPresidents.remove();
+        livingPresidents.remove();
+        livingPresidents.remove();
+        livingPresidents.poll();
+        livingPresidents.poll();
+        livingPresidents.remove();
+        livingPresidents.remove();
+        livingPresidents.remove();
+        System.out.println("Живые президенты США");
+        System.out.println(livingPresidents);
     }
 }
